670ef527762b1de8d45618295312a1b23acc5ff7
jmartinez729 <jmartinez729@gmail.com>
2018-11-29 16:55:44 -0500

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,18 @@
+create table projects (
+  id INTEGER PRIMARY KEY,
+  title text,
+  category text,
+  funding_goal integer,
+  start_date integer,
+  end_date integer);
+
+create table users (
+  id INTEGER PRIMARY KEY,
+  name text,
+  age integer);
+
+create table pledges (
+  id INTEGER PRIMARY KEY,
+  amount integer,
+  user_id integer,
+  project_id integer);
